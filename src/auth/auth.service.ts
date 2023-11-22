acc742e4cbb6cb245247eef256a0d0bc3f4ad9d6
Jean <jean.rioseco@gmail.com>
2023-11-21 22:09:21 -0300

    Arreglo de credenciales

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -61,7 +61,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({
       where: {email},
-      select: { email:true, password: true, id: true }
+      select: { email:true, id: true, fullName: true, isActive:true, roles:true, password:true}
       });
 
     if ( !user )
